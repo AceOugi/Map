84f7e373aa5dc2d83df84d4a5a1c10a96a740b5d
Ryan Di <git@aceougi.com>
2016-07-09 22:08:11 +0200

    Fix shell_exec path

--- map.php
+++ map.php
@@ -3,7 +3,7 @@
 $it = new RecursiveDirectoryIterator($argv[1], FilesystemIterator::SKIP_DOTS);
 
 foreach(new RecursiveIteratorIterator($it) as $file)
-    if ($result = json_decode(shell_exec('php map_file.php "'.$file.'"'), true))
+    if ($result = json_decode(shell_exec('php '.__DIR__.'/map_file.php "'.$file.'"'), true))
         $map = array_merge_recursive($result, $map ?? []);
 
 file_put_contents(($argv[2]??'.').'/routes.json', json_encode($map ?? []));
