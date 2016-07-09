3a6719c0087bf3b6de150cb4521fd772b6e633f4
Ryan Di <git@aceougi.com>
2016-07-09 22:13:51 +0200

    Add filtre only .php file

--- map.php
+++ map.php
@@ -3,7 +3,8 @@
 $it = new RecursiveDirectoryIterator($argv[1], FilesystemIterator::SKIP_DOTS);
 
 foreach(new RecursiveIteratorIterator($it) as $file)
-    if ($result = json_decode(shell_exec('php '.__DIR__.'/map_file.php "'.$file.'"'), true))
-        $map = array_merge_recursive($result, $map ?? []);
+    if (preg_match('/\.php$/', $file))
+        if ($result = json_decode(shell_exec('php '.__DIR__.'/map_file.php "'.$file.'"'), true))
+            $map = array_merge_recursive($result, $map ?? []);
 
 file_put_contents(($argv[2]??'.').'/routes.json', json_encode($map ?? []));
